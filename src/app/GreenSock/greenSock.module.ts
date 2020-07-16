e2bc2fbc96ebc214c5e2d0c204002ae9d5acc76e
Admin <krzysztoftota22@gmail.com>
2020-07-16 16:49:59 +0200

    skończenie animacji na stronie 4

--- src/app/GreenSock/greenSock.module.ts
+++ src/app/GreenSock/greenSock.module.ts
@@ -4,13 +4,19 @@ import {MatButtonModule} from '@angular/material/button';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import {CommonModule} from '@angular/common';
 import { ScrollPageComponent } from './scroll-page/scroll-page.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [SomethingComponent, PokedexComponent, ScrollPageComponent],
   exports: [SomethingComponent, PokedexComponent, ScrollPageComponent],
   imports: [
     MatButtonModule,
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
 
 })
--- src/app/GreenSock/scroll-page/scroll-page.component.ts
+++ src/app/GreenSock/scroll-page/scroll-page.component.ts
@@ -30,7 +30,7 @@ export class ScrollPageComponent implements OnInit {
   lastButton = [];
   lastWindow = [];
   hearts = [];
-  heartsR = [];
+  confetti = [];
   fullWindow = false;
   tl = gsap.timeline();
   buttons$ = new BehaviorSubject<Array<buttons>>([{
@@ -58,6 +58,13 @@ export class ScrollPageComponent implements OnInit {
           Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac,
           laoreet enim.`
   }]);
+  colorsPalette = [
+    ['#DC143C', '#B8860B', '#006400', '#8B008B', '#00008B', '#D2691E'],
+    ['#FF1493', '#00BFFF', '#FF4500', '#8B008B', '#FF0000', '#40E0D0'],
+    ['#FF00FF', '#BA55D3', '#C71585', '#8B008B', '#00008B', '#800080'],
+    ['#4682B4', '#87CEEB', '#4169E1', '#8B008B', '#0000CD', '#1E90FF'],
+  ];
+  colors = [];
 
   constructor(private elRef: ElementRef<HTMLElement>) {
   }
@@ -203,9 +210,14 @@ export class ScrollPageComponent implements OnInit {
     for (let i = 0; i < 100; i++) {
       this.hearts.push(i);
     }
+    for (let i = 0; i < 500; i++) {
+      this.confetti.push(i);
+    }
+    this.colors = this.colorsPalette[(Math.random() * this.colorsPalette.length) | 0];
   }
 
   animationBall(button) {
+
     if (!this.animationMap.has(button)) {
       const tl = gsap.timeline();
       tl.to(button, {
@@ -218,7 +230,7 @@ export class ScrollPageComponent implements OnInit {
     this.animationMap.get(button).play();
   }
 
-  onAnimationBall(button) {
+  onAnimation(button) {
     if (!this.click) {
       if (this.animationMap.has(button)) {
         this.animationMap.get(button).reverse();
@@ -272,38 +284,172 @@ export class ScrollPageComponent implements OnInit {
   fireR() {
     const heart = this.page.nativeElement.querySelectorAll('.heartR');
     for (let i = 0; i <= heart.length; i++) {
-      gsap.set(heart[i], {
-        x: 0, y: 0,
-      });
-      gsap.to(heart[i], {
-        physics2D: {
-          velocity: Math.random() * 200 + 400,
-          angle: -150,
-          gravity: Math.random() * 80 + 400,
-        },
-        scale: Math.random() + .2,
-        duration: 2.5,
-        delay: Math.random(),
-      });
+      if (heart[i]) {
+        gsap.set(heart[i], {
+          x: 0, y: 0,
+        });
+        gsap.to(heart[i], {
+          physics2D: {
+            velocity: Math.random() * 200 + 400,
+            angle: -150,
+            gravity: Math.random() * 80 + 400,
+          },
+          scale: Math.random() + .2,
+          duration: 2.5,
+          delay: Math.random(),
+        });
+      }
     }
   }
 
   fireL() {
     const heart = this.page.nativeElement.querySelectorAll('.heartL');
     for (let i = 0; i <= heart.length; i++) {
-      gsap.set(heart[i], {
-        x: 0, y: 0,
-      });
-      gsap.to(heart[i], {
-        physics2D: {
-          velocity: Math.random() * 200 + 400,
-          angle: -30,
-          gravity: Math.random() * 80 + 400,
-        },
-        scale: Math.random() + .2,
-        duration: 2.5,
-        delay: Math.random(),
-      });
+      if (heart[i]) {
+        gsap.set(heart[i], {
+          x: 0, y: 0,
+          scale: Math.random() + .2,
+        });
+        gsap.to(heart[i], {
+          physics2D: {
+            velocity: Math.random() * 200 + 400,
+            angle: -30,
+            gravity: Math.random() * 80 + 400,
+          },
+          duration: 2.5,
+          delay: Math.random(),
+        });
+      }
     }
+  }
+
+  animationButton(button) {
+    if (!this.animationMap.has(button)) {
+      const tl = gsap.timeline();
+      tl.to(button, {
+        backgroundColor: 'green',
+        duration: 1,
+      });
+      this.animationMap.set(button, tl);
+    }
+
+    this.animationMap.get(button).play();
+  }
+
+  bigBoom() {
+    this.click = true;
+    const windowBoom = (this.page.nativeElement.querySelectorAll('.windowBoom, .box, .boxXL'));
+    const button = (this.page.nativeElement.querySelector('.boom'));
+    const caboom = (this.page.nativeElement.querySelector('.caboom'));
+    const shadow = (this.page.nativeElement.querySelectorAll('.shadow'));
+    const head = (this.page.nativeElement.querySelector('.head'));
+    const fire = (this.page.nativeElement.querySelector('.fire'));
+    const confetti = this.page.nativeElement.querySelectorAll('.confetti');
+    const textWindow = (this.page.nativeElement.querySelector('.textWindow'));
+    const textLine = (this.page.nativeElement.querySelector('.textLine'));
+    const ale = (this.page.nativeElement.querySelector('.ale'));
+    const sie = (this.page.nativeElement.querySelector('.sie'));
+    const dzialo = (this.page.nativeElement.querySelector('.dzialo'));
+    const tl = gsap.timeline();
+    tl.to(button, {
+      cursor: 'auto',
+      rotation: 90,
+      backgroundColor: 'grey',
+      transformOrigin: 'left top',
+      x: 100,
+      duration: 1,
+    })
+      .to(shadow, {
+        visibility: 'visible'
+      }, '<')
+      .to(button, {
+        y: -150,
+        duration: 1,
+      })
+      .to(head, {
+        y: -225,
+        duration: 1,
+      }, '<')
+      .to(fire, {
+        transformOrigin: 'right, top',
+        scale: 1.3,
+      })
+      .to(fire, {
+        transformOrigin: 'right, bottom',
+        scale: Math.random() * .3 + 1,
+        yoyo: true,
+        repeat: -1
+      })
+      .to(button, {
+        y: -450,
+        duration: 1,
+      }, '+=1')
+      .to(head, {
+        y: -525,
+        duration: 1,
+      }, '<')
+      .to(fire, {
+        y: -300,
+        duration: 1,
+      }, '<');
+
+    for (let i = 0; i <= confetti.length; i++) {
+      const color = this.colors[(Math.random() * this.colors.length) | 0];
+      if (confetti[i]) {
+        tl.set(confetti[i], {
+          y: Math.random() * 70,
+          x: 0,
+          backgroundColor: color,
+          visibility: 'visible',
+          scale: Math.random() + .9,
+        }, '-<.5');
+        tl.to(confetti[i], {
+          physics2D: {
+            velocity: Math.random() * 300 + 200,
+            angle: Math.random() * 300 + 100,
+            gravity: Math.random() * 80 + 400,
+          },
+          duration: 4,
+          delay: Math.random(),
+          rotateX: Math.random() * 360,
+          rotateY: Math.random() * 360,
+          rotateZ: Math.random() * 360,
+        }, '-<i');
+      }
+    }
+    tl.to(caboom, {
+      filter: 'blur(0px)',
+      transformOrigin: 'center center',
+      scale: 2,
+      duration: 5,
+    }, '-=4')
+      .to(windowBoom, {
+        x: 5,
+        duration: .1,
+        yoyo: true,
+        repeat: 46,
+      }, '<')
+      .to(textWindow, {
+        visibility: 'visible',
+        duration: .1,
+      }, '+=3')
+      .to(textLine, {
+        width: '90%',
+        duration: 2,
+      }, '-<3')
+      .to(ale, {
+        duration: 2,
+        rotateX: 360,
+      })
+      .to(sie, {
+        y: '100%',
+        duration: 2,
+      })
+      .from(dzialo, {
+        y: 2500,
+        x: 2500,
+        duration: 2,
+        scale: 10,
+      });
   }
 }
